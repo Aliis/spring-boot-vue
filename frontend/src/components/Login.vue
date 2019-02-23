ed8242a6b48671566c0e2ea34196eda71f65ba2f
Aliis Kiiker <aliis.kiiker@fleet.zone>
2019-02-23 18:00:24 +0200

    Version 0.2

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-dialog
+            v-model="dialog"
+            width="500"
+    >
+        <v-btn
+            color="blue lighten-2"
+            dark
+            @click="dialog = true" flat slot="activator" >Login
+        </v-btn>
+        <v-card>
+
+            <user/>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                        color="primary"
+                        flat
+                        @click="dialog = false"
+                >
+                    Close
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    import user from './LoginModalContent'
+    export default {
+        name: 'signinmodal',
+        components: {
+            user
+        },
+        data () {
+            return {
+                dialog: false
+            }
+        }
+    }
+</script>
---
+++ frontend/src/views/MainFrame.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+        <login />
+    </v-container>
+</template>
+
+<script>
+    import login from '../components/Login.vue'
+  export default {
+      name: 'home',
+      components: {
+          login
+      }
+  }
+</script>
+
